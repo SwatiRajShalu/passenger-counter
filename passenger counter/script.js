8ba1ebb30550ea0d590ec7b13a31b5b19d664511
Sagar ray <raysagar807@gmail.com>
2023-09-02 11:59:47 +0530

    added features like localstorag , disabled btn , clearbtn

--- passenger counter/script.js
+++ passenger counter/script.js
@@ -1,17 +1,92 @@
 let countel=document.getElementById("count-el")
+let listTasks = document.getElementById('list');
+let entries=document.getElementById("entries-el");
+let savebtn=document.getElementById("save-el");
 let count=0
-let entries=document.getElementById("entries-el")
+
 function increament(){
+    
     count+=1
     countel.textContent=count
-}
-function decreament(){
-    count-=1
-    countel.textContent=count
-}
-function save(){
-    entries.textContent+=count+" - "
-    count=0
-    countel.textContent=count
+
+    if(count==0)
+    savebtn.disabled = true;
+    else
+    savebtn.disabled = false;
 
 }
+function decreament(){
+    
+    
+
+    if(count==0)
+    return ;
+
+    count-=1
+    countel.textContent=count
+
+    if(count==0)
+    savebtn.disabled = true;
+    else
+    savebtn.disabled = false;
+}
+
+
+let tasks = []
+
+function saveTaskList() {
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function retrieveList() {
+
+    if(count==0)
+    savebtn.disabled = true;
+    
+  let retrievedTasks = localStorage.getItem("tasks");
+  if (retrievedTasks) {
+    tasks = JSON.parse(retrievedTasks);
+  }
+}
+
+function clearTaskList() {
+  tasks = [];
+  renderTaskList();
+  saveTaskList();
+}
+
+function renderTaskList() {
+  
+    if(count==0)
+    savebtn.disabled = true;
+
+  listTasks.innerHTML = "";
+  for (let i = 0; i < tasks.length; i++) {
+    let li = document.createElement('li');
+    li.innerText = tasks[i];
+    listTasks.appendChild(li);
+  }
+}
+
+function addTask() {
+
+  let task = count;
+  count=0;
+  if(tasks.length>9)
+  tasks.shift();
+
+  tasks.push(task);
+  renderTaskList();
+  saveTaskList();
+}
+
+function save() {
+
+  countel.textContent=0;
+  addTask();
+}
+
+
+retrieveList();
+renderTaskList();
